4e5a830fd28ddd9fcc57ea5466514b9818c4d3cd
manoloIESvirgendelcarmen <manu.chinaski@gmail.com>
2015-11-10 20:55:44 +0100

    Cambiado nombre de archivo

--- Interes.java
+++ Interes.java
@@ -4,8 +4,12 @@
 // al tiempo (t), y a la tasa de interés (i) :
 //I = C · i · t
 
-
-public static double calcularInter(argumentos){
+public class Interes{
+public static double calcularInter(double C, double i, double t){
    //código para calcular el interes
+   double interes=0;
+   interes= C*i*t;
    //return interes
+	return interes;
+	}
 }
---
+++ TesInteres.java
@@ -0,0 +1,23 @@
+//fichero Interes.java
+//leemos desde un Scanner los valores
+//capital, tiempo y tasa de  interés
+//y mediante el  método calcularInteres de la
+//clase  Interes muestra por pantalla el interés
+//calculado
+
+import java.util.Scanner;
+public class TesInteres{
+   public static void main(String[] arg){
+	   Scanner banco=new Scanner (System.in);
+        //preparamos el scanner
+        System.out.println("Introduce el valor del capital, tiempo y tasa de interés: ");
+        //lectura de lo datos
+        double capital=banco.nextDouble();
+        double interes=banco.nextDouble();
+        double tiempo=banco.nextDouble();
+        //llamada al método calcularInteres
+        System.out.println("El interes proporcional es: "+Interes.calcularInter(capital,interes,tiempo));
+        //mostramos por pantalla el valor del interés
+       
+   }
+}
